a1130ed1a81595126231aea12c8e72a8c03cd51d
glcarranzani <glcarranzani@gmail.com>
2022-12-13 00:04:45 -0300

    Hover creado segun su estado - proyectoCards

--- src/components/Proyectos/Proyectos.js
+++ src/components/Proyectos/Proyectos.js
@@ -1,11 +1,21 @@
 import './Proyectos.css'
 import '../styles/generalStyles.css'
 import Container from 'react-bootstrap/Container';
+import ProyectosCards from '../ProyectosCards/ProyectosCards';
 
 const Proyectos = () => {
+
+    const ProyectosRealizados = [
+        { id: 1, title: 'AFK Tecnología', img: './assets/img/afk-tecnologia-project.png', description: 'Ecommerce de productos para PC' },
+        { id: 2, title: 'Entre Copas', img: './assets/img/entre-copas-project.png', description: 'Diseño de página acerca del vino' }
+    ]
+
     return (
-        <Container className='containerGeneral' id='proyectos'>
+        <Container className='containerProjects' id='proyectos'>
             <h2> Proyectos</h2 >
+            <div className='containerProjectsCards'>
+                {ProyectosRealizados.map(card => <ProyectosCards key={card.id}{...card} />)}
+            </div>
         </Container>
     )
 }
