70871d6d884fec454161cf75054a72d9483f9c79
glcarranzani <glcarranzani@gmail.com>
2022-12-23 19:44:46 -0300

    Se termina de corregir el hover creado con los eventos del mouse

--- src/components/ProyectosCards/ProyectosCards.js
+++ src/components/ProyectosCards/ProyectosCards.js
@@ -3,30 +3,34 @@ import Card from 'react-bootstrap/Card';
 import { BsGithub } from 'react-icons/bs'
 import { BsBoxArrowUpRight } from 'react-icons/bs'
 import { useState } from 'react';
+import 'animate.css'
 
 
 const ProyectosCards = ({ img, title, id, description }) => {
     const [card, setCard] = useState(false)
+
+    const [color, setColor] = useState(false)
+    const colorA = 'animate__animated animate__fadeInUp'
+    const colorB = 'animate__animated animate__fadeOutDown'
 
 
     return (
         <Card
             className="bg-dark text-white projectCard"
             key={id}
-            onMouseEnter={() => { setCard(true); }}
-            onMouseLeave={() => { setCard(false); }}
+            onMouseEnter={() => { setCard(true); setColor(true) }}
+            onMouseLeave={() => { setCard(false); setColor(true) }}
         >
-            <Card.Img src={img} alt={title} className={card ? 'imgCardProject' : 'hidden'} />
-            {/* <Card.Img src={img} alt={title} className='imgCardProject' /> */}
-            <Card.ImgOverlay className='containerText'>
-                <div>
+            <Card.Img src={img} alt={title} className={card ? 'imgCardBlur' : 'hidden'} />
+            <Card.ImgOverlay className={color ? 'visible, containerText' : 'oculto'}>
+                <div className={card ? colorA : colorB}>
                     <p>{title}</p>
                     <p>{description}</p>
                     <span className='footerIcon p-2'> <BsGithub /> </span>
                     <span className='footerIcon p-2'> <BsBoxArrowUpRight /> </span>
                 </div>
             </Card.ImgOverlay>
-        </Card>
+        </Card >
     )
 }
 
