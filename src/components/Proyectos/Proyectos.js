b96c1b54546242bbd848dc38e8bcbbf94c1323eb
glcarranzani <glcarranzani@gmail.com>
2022-12-28 07:23:33 -0300

    Se agrega nuevo proyecto y agregue los enlaces correspondientes a cada proyecto

--- src/components/Proyectos/Proyectos.js
+++ src/components/Proyectos/Proyectos.js
@@ -6,8 +6,9 @@ import ProyectosCards from '../ProyectosCards/ProyectosCards';
 const Proyectos = () => {
 
     const ProyectosRealizados = [
-        { id: 1, title: 'AFK Tecnología', img: './assets/img/afk-tecnologia-project.png', description: 'Ecommerce de productos para PC' },
-        { id: 2, title: 'Entre Copas', img: './assets/img/entre-copas-project.png', description: 'Diseño de página acerca del vino' }
+        { id: 1, title: 'AFK Tecnología', img: './assets/img/afk-tecnologia-project.png', description: 'Ecommerce de productos para PC', url: 'https://afktecnologia.vercel.app/', github: 'https://github.com/GianinaLC/afktecnologia' },
+        { id: 2, title: 'Entre Copas', img: './assets/img/entre-copas-project.png', description: 'Diseño de página acerca del vino', url: 'https://gianinalc.github.io/DesarrolloWeb/Proyecto/', github: 'https://github.com/GianinaLC/DesarrolloWeb' },
+        { id: 3, title: 'TODO list', img: './assets/img/todo-list-project.png', description: 'Lista de tareas', url: 'https://gianinalc.github.io/todo-list/', github: 'https://github.com/GianinaLC/todo-list' }
     ]
 
     return (
