b5c852befc9f79765f804ce18359b3016e71bf10
glcarranzani <glcarranzani@gmail.com>
2023-01-04 17:47:05 -0300

    Se realizo el responsive del componente

--- src/components/Formacion/Formacion.js
+++ src/components/Formacion/Formacion.js
@@ -4,7 +4,7 @@ import FormacionCard from '../FormacionCard/FormacionCard';
 
 const Formacion = () => {
     return (
-        <Container fluid id='Formación'>
+        <Container fluid id='Formación' className='firstBgColor'>
             <h2 className='marginSecondary'>Formación</h2>
             <div className='divSec'>
                 <FormacionCard />
